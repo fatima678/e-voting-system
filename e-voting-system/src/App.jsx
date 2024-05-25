3c511d6e3ee40ebccd6c870045733cc42948e8dd
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-05-25 23:46:26 +0500

    Initial commit

---
+++ e-voting-system/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import Registration from './components/Registration/Registration'
+import Login from './components/Registration/Login'
+
+function App() {
+
+
+  return (
+    <>
+   
+    <Registration/>
+    <Login/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ e-voting-system/src/components/Registration/Registration.jsx
@@ -0,0 +1,86 @@
+
+import React from "react";
+import vote from '../../assets/vote.jpg';
+
+export default function Registration() {
+  return (
+    <div
+      className="min-h-screen py-16"
+      style={{ backgroundImage: "linear-gradient(115deg, #9F7AEA, #FEE2FE)" }}
+    >
+      <div className="container mx-auto">
+        <div className="flex w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
+          <div
+            className="w-1/2 flex flex-col items-center justify-center p-8 bg-no-repeat bg-cover bg-center"
+            style={{ backgroundImage: `url(${vote})` }}
+          >
+            <h1 className="text-white text-3xl font-bold mb-3">Welcome</h1>
+            <div>
+              <p className="text-white font-semibold">
+                An online voting system is a software platform that allows
+                groups to securely conduct votes and elections. High-quality
+                online voting systems balance ballot security, accessibility,
+                and the overall requirements of an organization's voting event.
+              </p>
+            </div>
+          </div>
+          <div className="py-14 px-10 w-1/2">
+            <h2 className="text-3xl mb-1 font-bold">Voter Registration</h2>
+            <p className="mb-4 text-gray-500 font-light">
+              Create your account. It's free and only takes a minute
+            </p>
+            <form>
+              <div className="grid grid-cols-2 gap-3">
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  className="border border-gray-400 py-1 px-2"
+                />
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  className="border border-gray-400 py-1 px-2"
+                />
+              </div>
+              <div className="mt-4">
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="border border-gray-400 py-1 px-2 w-full"
+                />
+              </div>
+              <div className="mt-4">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="border border-gray-400 py-1 px-2 w-full"
+                />
+              </div>
+              <div className="mt-4">
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  className="border border-gray-400 py-1 px-2 w-full"
+                />
+              </div>
+              <div className="mt-2">
+                <input type="checkbox" className="border border-gray-400" />
+                <span>
+                  I accept the{" "}
+                  <a className="text-purple-400 font-semibold">Terms of Use</a>{" "}
+                  &{" "}
+                  <a className="text-purple-400 font-semibold">Privacy Policy</a>
+                </span>
+              </div>
+              <div className="mt-5">
+                <button className="w-full bg-purple-400 py-3 text-center text-white font-medium">
+                  Register Now
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
