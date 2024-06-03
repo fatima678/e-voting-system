77627c7bae208930736f9fccdf1506b12060758d
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-06-04 01:10:03 +0500

    update the code of app.jsx

--- e-voting-system/src/App.jsx
+++ e-voting-system/src/App.jsx
@@ -3,34 +3,44 @@ import Navbar from './components/Navbar'; // Import the Navbar component
 import Registration from './components/Registration';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard'; 
-// import CandidatesList from "./components/CandidatesList";
 import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
 import Services from './components/Services';
 import CandidateProfile from './components/CandidateProfile';
 import CandidateRegistration from './components/CandidateRegistration';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      {/* Navbar */}
-      <Navbar />
+      <Main />
+    </Router>
+  );
+}
+
+function Main() {
+  const location = useLocation();
+
+  const noNavBarRoutes = ["/", "/candidateRegistration", "/login"];
+  const shouldShowNavBar = !noNavBarRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {/* Conditionally render Navbar */}
+      {shouldShowNavBar && <Navbar />}
 
       {/* Routes */}
       <Routes>
         <Route path="/" element={<Registration />} />
-        <Route path="candidateRegistration" element={<CandidateRegistration />} />
+        <Route path="/candidateRegistration" element={<CandidateRegistration />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} /> 
-        {/* <Route path="/candidatesList" element={<CandidatesList />} />  */}
         <Route path="/aboutUs" element={<AboutUs />} /> 
         <Route path="/contactUs" element={<ContactUs />} /> 
         <Route path="/services" element={<Services />} /> 
         <Route path="/profile/:id" element={<CandidateProfile />} />
-
       </Routes>
-    </Router>
+    </>
   );
 }
 
